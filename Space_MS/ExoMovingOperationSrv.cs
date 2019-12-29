66d69f74a90b525fca07a1e5e722744cb1f49328
aulusjpn <ryugensui@gmail.com>
2019-12-29 14:40:44 +0900

    プロジェクト修復

---
+++ Space_MS/ExoMovingOperationSrv.cs
@@ -0,0 +1,41 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class ExoMovingOperationSrv : OperationServiceBase
+        {
+            public ExoMovingOperationSrv(LegModel rLeg, LegModel lLeg, ArmModel rArm, ArmModel lArm) : base(rLeg, lLeg, rArm, lArm)
+            {
+            }
+
+            public override void armTarget(IMyCockpit Rota)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Drive(IMyCockpit cockpit)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Space_MS/Program.MotorOperationDataEntity.cs
@@ -0,0 +1,81 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class MotorOperationDataModel
+        {
+            private float angle;
+
+            public float GetAngle()
+            {
+                return angle;
+            }
+
+            public void SetAngle(float value)
+            {
+                angle = value;
+            }
+
+            private float velocity;
+
+            public float GetVelocity()
+            {
+                return velocity;
+            }
+
+            public void SetVelocity(float value)
+            {
+                velocity = value;
+            }
+
+            private bool reverce;
+
+            public bool GetReverce()
+            {
+                return reverce;
+            }
+
+            public void SetReverce(bool value)
+            {
+                reverce = value;
+            }
+
+            IMyMotorStator MotorStator;
+
+            MotorOperationDataModel (IMyMotorStator motorStator)
+            {
+               MotorStator = motorStator;
+            }
+
+
+
+            public MotorOperationDataModel getData(IMyMotorStator myMotor)
+            {
+                string text = myMotor.CustomData;
+
+                var arr = text.Split(Environment.NewLine.ToArray(), StringSplitOptions.None);
+
+                return this;
+            }
+        }
+    }
+}
